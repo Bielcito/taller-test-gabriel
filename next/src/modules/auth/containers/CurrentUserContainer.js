5e8b2960b52cd26537bdb091f0954a2435a0ddab
Gabriel Rocha de Oliveira <oliveira.grdo@gmail.com>
2018-05-18 17:00:01 -0300

    #14 - SOLVED - Redirecionar usuário anônimo

--- next/src/modules/auth/containers/CurrentUserContainer.js
+++ next/src/modules/auth/containers/CurrentUserContainer.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { func } from 'prop-types'
 import gql from 'graphql-tag'
 import { Query } from 'react-apollo'
+import Router from 'next/router'
 
 import { isClient } from 'app/lib/func'
 
@@ -16,16 +17,27 @@ const query = gql`
 `
 
 let refetchedOnClient = false
+let userDataObtained = false
 
 const CurrentUserContainer = ({ children }) => (
   <Query query={ query }>
-    { ({ ...result, loading, refetch, data }) => {
+    { ({ ...result, loading, refetch, data, networkStatus }) => {
       // Force a refetch on the client inside to make sure
       // the cached SSR anonymous user is replaced, in case
       // the user is already logged in..
       if (!loading && !refetchedOnClient && isClient()) {
         refetchedOnClient = true
         refetch()
+      }
+
+      if (userDataObtained) {
+        if (data.user.uid === null) {
+          Router.push('/')
+        }
+      }
+
+      if (loading && refetchedOnClient && !userDataObtained) {
+        userDataObtained = true
       }
 
       return children({ ...result, user: data.user })
